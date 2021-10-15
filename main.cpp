47018191a25d02332f2e59d97d53471b138b50af
DoroninYta <89692985+DoroninYta@users.noreply.github.com>
2021-10-15 11:48:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,59 @@
-/// @file main.cpp
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
 
-#include "doc.h"
+const double EPSILON = 1e-7;
 
-int isEqual(double a, double b)									// сравнение чисел разных типов
+enum nRoots {
+	NO_ROOTS = 0,
+	ONLY_ONE_ROOT = 1,
+	TWO_ROOTS = 2,
+	INF_ROOTS = 3,
+	ERROR = 4
+};
+
+
+/*РСЃРїСЂР°РІР»РµРЅРёСЏ:
+* С„СѓРЅРєС†РёРё РЅР°РїРёСЃР°РЅС‹ РґР»СЏ СЋРЅРёС‚РµСЃС‚РѕРІ
+* РёР·РјРµРЅРµРЅР° С„СѓРЅРєС†РёСЏ equal
+* assert РґРѕР±Р°Р»РµРЅ Рє РєР°Р¶РґРѕР№ С„СѓРЅРєС†РёРё, РіРґРµ РµСЃС‚СЊ СѓРєР°Р·Р°С‚РµР»Рё
+*/
+
+
+int is_equal(double a, double b)
 {
-	if (fabs(a - b) < EPSILON)
-		return 1;
+	if ((a / EPSILON <= 10) || (b / EPSILON) <= 10)
+	{
+		if ((a - b) == 0)
+			return 1;
+		else
+			return 0;
+	}
 	else
-		return 0;
+	{
+		if (fabs(a - b) <= EPSILON)
+			return 1;
+		else
+			return 0;
+	}
 }
 
 
-int fn_safe_scanf(int n_Try_enter, double* a, double* b, double* c)			//функция безопастного ввода символов с клавиатуры
+
+int fn_safe_scanf(int n_Try_enter, double* a, double* b, double* c)			//С„СѓРЅРєС†РёСЏ Р±РµР·РѕРїР°СЃС‚РЅРѕРіРѕ РІРІРѕРґР° СЃРёРјРІРѕР»РѕРІ СЃ РєР»Р°РІРёР°С‚СѓСЂС‹
 {
-	int nCharacters = scanf("%lf %lf %lf", a, b, c);
+	assert(a != b != NULL);
+	assert(a != c != NULL);
+	assert(b != c != NULL);
+	
+	int nCharacters = scanf_s("%lf %lf %lf", a, b, c);
 	int nTry = 0;
 
 	while (nTry < n_Try_enter && nCharacters != 3)
 	{
-		while (getchar() != '\n') { ; }										//отчистка буфера
+		while (getchar() != '\n') { ; }										//РѕС‚С‡РёСЃС‚РєР° Р±СѓС„РµСЂР°
 		printf("error, please, try again, you have %d tryes to enter\n", n_Try_enter - nTry);
-		nCharacters = scanf("%lf %lf %lf", a, b, c);
+		nCharacters = scanf_s("%lf %lf %lf", a, b, c);
 		nTry++;
 	}
 
@@ -43,13 +76,14 @@ int fn_sq_solver(double a, double b, double c, double* x1, double* x2)
 {
 	assert(x1 != NULL);
 	assert(x2 != NULL);
+	assert(x1 != x2);
 
-	if (isEqual(a, 0) == 1)
+	if (is_equal(a, 0) == 1)
 	{
 
-		if (isEqual(b, 0) == 1)
+		if (is_equal(b, 0) == 1)
 		{
-			if (isEqual(c, 0) == 1)
+			if (is_equal(c, 0) == 1)
 				return (INF_ROOTS);
 
 			else
@@ -74,7 +108,7 @@ int fn_sq_solver(double a, double b, double c, double* x1, double* x2)
 
 			return (TWO_ROOTS);
 		}
-		else if (isEqual(D, 0) == 1)
+		else if (is_equal(D, 0) == 1)
 		{
 			*x1 = -b / 2 / a;
 
@@ -89,9 +123,9 @@ int fn_sq_solver(double a, double b, double c, double* x1, double* x2)
 
 }
 
-int PrintfResult(int nRoots)											// для вывода количества корней
+int print_result(int nRoots)											// РґР»СЏ РІС‹РІРѕРґР° РєРѕР»РёС‡РµСЃС‚РІР° РєРѕСЂРЅРµР№
 {
-	switch (nRoots) {		// z переделать
+	switch (nRoots) {		
 	case NO_ROOTS:
 		printf("no roots\n");
 		break;
@@ -110,164 +144,94 @@ int PrintfResult(int nRoots)											// для вывода количества корней
 	return 0;
 }
 
-int Uni_Test_sqrt_solver()
+
+void uni_test_sqrt_solver_NO_ROOTS_or_INF_ROOTS(int count_roots_from_function, int n_roots_must_be, int test_number,\
+	double a, double b, double c)
 {
-	double a = NAN, b = NAN, c = NAN;
-	double x1 = NAN, x2 = NAN;
-	double res1 = NAN, res2 = NAN;
-
-// uni test 1
-
-	a = 1;
-	b = -2;
-	c = 1;
-	res1 = 1;
-
-	int root_count = fn_sq_solver(a, b, c, &x1, &x2);
-	if (!(isEqual(x1, res1) && root_count == 1))
+	if (count_roots_from_function != n_roots_must_be)
 	{
-		printf("error test 1: a = %lf, b = %lf, c = %lf;\n "
-			"x1 = %lf, count of roots = %d\n"
-			"should be x1 = %lf, count of roots = 1\n", a, b, c, x1, root_count, res1);
+		printf("error test в„–%d: a = %lf, b = %lf, c = %lf;\n "
+			"count of roots must be %d\n"
+			"int the result of program work %d \n", test_number, a, b, c,\
+			n_roots_must_be, count_roots_from_function);
 	}
-	//uni test 2
-	x1 = NAN, x2 = NAN;
-	res1 = NAN, res2 = NAN;
-	a = 1;
-	b = -5;
-	c = 6;
-	res1 = 2;
-	res2 = 3;
+}
 
-	root_count = fn_sq_solver(a, b, c, &x1, &x2);
-	if (!(((isEqual(x1, res1) == 1 && isEqual(res2, x2) == 1) || (isEqual(res1, x2) && isEqual(res2, x1)))\
-		&& root_count == TWO_ROOTS))
+
+void uni_test_sqrt_solver_ONLY_ONE_ROOT(int count_roots_from_function, int n_roots_must_be, int test_number, \
+	double a, double b, double c, double x1, double res1)
+{
+	if (count_roots_from_function != n_roots_must_be || x1 != res1)
 	{
-		printf("error test 2: a = %lf, b = %lf, c = %lf;\n "
-			"x1 = %lf, x2 = %lf, count of roots = %d\n"
-			"should be x1 = %lf, x2 = %lf, count of roots = 2\n", a, b, c, x1, x2, root_count, res1, res2);
+		printf("error test в„–%d: a = %lf, b = %lf, c = %lf;\n "
+			"count of roots must be %d\n"
+			"int the result of program works %d \n"
+			"roots : \n program work result x1 = %lf\n"
+			"x1 should be = %lf", test_number, a, b, c, n_roots_must_be, \
+			count_roots_from_function, x1, res1);
 	}
+}
 
-	//uni test 3
 
-	x1 = NAN, x2 = NAN;
-	res1 = NAN, res2 = NAN;
-	a = 0;
-	b = 0;
-	c = 0;
-	res1 = NAN;
-	res2 = NAN;
-
-	root_count = fn_sq_solver(a, b, c, &x1, &x2);
-	if (!( root_count == INFINITY) && x1 == NAN && x2 == NAN)
+void uni_test_sqrt_solver_TWO_ROOTS(int count_roots_from_function, int n_roots_must_be, \
+									   int test_number, double a, double b, double c, double x1,\
+									   double x2, double res1, double res2)
+{
+	if(!((is_equal(x1,res1) && is_equal(x2, res2)) || (is_equal(x1,res2) && is_equal(x2,res1))))
 	{
-		printf("error test : a = %lf, b = %lf, c = %lf;\n "
-			"x1 = %lf, x2 = %lf, count of roots = %d\n"
-			"should be x1 = %lf, x2 = %lf, count of roots = 3\n", a, b, c, x1, x2, root_count, res1, res2);
+		printf("error test в„–%d: a = %lf, b = %lf, c = %lf;\n "
+			"count of roots must be %d\n"
+			"int the result of program work %d \n"
+			"roots : \n program work result x1 = %lf; x2 = %lf\n"
+			"x1 should be = %lf; x2 should be = %lf\n", test_number, a, b, c, n_roots_must_be, \
+			count_roots_from_function, x1, x2, res1, res2);
+	}
+}
+
+
+int uni_test_sqrt_solver_main(int test_number, double a, double b, double c, int n_roots_must_be, double res1, \
+	double res2, double x1, double x2)
+{
+	int count_roots_function = fn_sq_solver(a, b, c, &x1, &x2);
+	switch (n_roots_must_be)
+	{
+	case NO_ROOTS:
+		uni_test_sqrt_solver_NO_ROOTS_or_INF_ROOTS(count_roots_function, n_roots_must_be, test_number, a, b, c);
+		break;
+	case ONLY_ONE_ROOT:
+		uni_test_sqrt_solver_ONLY_ONE_ROOT(count_roots_function, n_roots_must_be, test_number, a, b, c, x1, res1);
+		break;
+	case TWO_ROOTS:
+		uni_test_sqrt_solver_TWO_ROOTS(count_roots_function, n_roots_must_be, test_number, a, b, c, \
+			x1, x2, res1, res2);
+		break;
+	case INF_ROOTS:
+		uni_test_sqrt_solver_NO_ROOTS_or_INF_ROOTS(count_roots_function, n_roots_must_be, test_number, a, b, c);
+		break;
+
+
+	default:
+		printf("ERROR");
+		break;
 	}
 	return 0;
+}
 
-	//uni test 4
-	x1 = NAN, x2 = NAN;
-	res1 = NAN, res2 = NAN;
-	a = 24;
-	b = -527;
-	c = 17;
-	res1 = 21.926027740102793;
-	res2 = 0.03230559323054175;
 
-	root_count = fn_sq_solver(a, b, c, &x1, &x2);
-	if (!(((isEqual(x1, res1) == 1 && isEqual(res2, x2) == 1) || (isEqual(res1, x2) && isEqual(res2, x1)))\
-		&& root_count == TWO_ROOTS))
-	{
-		printf("error test 4: a = %lf, b = %lf, c = %lf;\n "
-			"x1 = %lf, x2 = %lf, count of roots = %d\n"
-			"should be x1 = %lf, x2 = %lf, count of roots = 2\n", a, b, c, x1, x2, root_count, res1, res2);
-	}
-
-	//uni test 5
-	x1 = NAN, x2 = NAN;
-	res1 = NAN, res2 = NAN;
-	a = 448;
-	b = 0;
-	c = -87808;
-	res1 = 14;
-	res2 = -14;
-
-	root_count = fn_sq_solver(a, b, c, &x1, &x2);
-	if (!(((isEqual(x1, res1) == 1 && isEqual(res2, x2) == 1) || (isEqual(res1, x2) && isEqual(res2, x1)))\
-		&& root_count == TWO_ROOTS))
-	{
-		printf("error test 5: a = %lf, b = %lf, c = %lf;\n "
-			"x1 = %lf, x2 = %lf, count of roots = %d\n"
-			"should be x1 = %lf, x2 = %lf, count of roots = 2\n", a, b, c, x1, x2, root_count, res1, res2);
-	}
-
-	//uni test 6
-	x1 = NAN, x2 = NAN;
-	res1 = NAN, res2 = NAN;
-	a = 448;
-	b = 0;
-	c = 87808;
-
-	root_count = fn_sq_solver(a, b, c, &x1, &x2);
-	if (!(x1 == NAN && x2 == NAN && root_count == TWO_ROOTS))
-	{
-		printf("error test 6: a = %lf, b = %lf, c = %lf;\n "
-			"x1 = %lf, x2 = %lf, count of roots = %d\n"
-			"should be x1 = %lf, x2 = %lf, count of roots = 3\n", a, b, c, x1, x2, root_count, res1, res2);
-	}
-
-	//uni test 7
-	x1 = NAN, x2 = NAN;
-	res1 = NAN, res2 = NAN;
-	a = 126;
-	b = 23;
-	c = 878;
-
-	root_count = fn_sq_solver(a, b, c, &x1, &x2);
-	if (!(x1 == NAN && x2 == NAN && root_count == NO_ROOTS))
-
-	{
-		printf("error test 7: a = %lf, b = %lf, c = %lf;\n "
-			"x1 = %lf, x2 = %lf, count of roots = %d\n"
-			"should be x1 = %lf, x2 = %lf, count of roots = 0\n", a, b, c, x1, x2, root_count, res1, res2);
-	}
-
-	//uni test 7
-	x1 = NAN, x2 = NAN;
-	res1 = NAN, res2 = NAN;
-	a = 1;
-	b = -10;
-	c = 24;
-	res1 = 6;
-	res2 = 4;
-
-	root_count = fn_sq_solver(a, b, c, &x1, &x2);
-	if (!(((isEqual(x1, res1) == 1 && isEqual(res2, x2) == 1) || (isEqual(res1, x2) && isEqual(res2, x1)))\
-		&& root_count == TWO_ROOTS))
-	{
-		printf("error test 7: a = %lf, b = %lf, c = %lf;\n "
-			"x1 = %lf, x2 = %lf, count of roots = %d\n"
-			"should be x1 = %lf, x2 = %lf, count of roots = 2\n", a, b, c, x1, x2, root_count, res1, res2);
-	}
-
-	//uni test 8
-	x1 = NAN, x2 = NAN;
-	res1 = NAN, res2 = NAN;
-	a = 0;
-	b = 448;
-	c = -87808;
-	res1 = 196;
-
-	root_count = fn_sq_solver(a, b, c, &x1, &x2);
-	if (!(((isEqual(x1, res1) == 1 && isEqual(res2, x2) == 1) || (isEqual(res1, x2) && isEqual(res2, x1)))\
-		&& root_count == TWO_ROOTS))
-	{
-		printf("error test 5: a = %lf, b = %lf, c = %lf;\n "
-			"x1 = %lf, x2 = %lf, count of roots = %d\n"
-			"should be x1 = %lf, x2 = %lf, count of roots = 2\n", a, b, c, x1, x2, root_count, res1, res2);
-	}
+int ft_uni_tests()
+{
+	double x1 = NAN, x2 = NAN;
+	uni_test_sqrt_solver_main(1, 1, -2, 1, 1, 1, NAN, x1, x2);
+	uni_test_sqrt_solver_main(2, 1, -5, 6, 2, 2, 3, x1, x2);
+	uni_test_sqrt_solver_main(3, 0, 0, 0, 3, NAN, NAN, x1, x2);
+	uni_test_sqrt_solver_main(4, 17, -79, 91, 2, 2.5376502908611918, 2.10940853266822, x1, x2);
+	uni_test_sqrt_solver_main(5, 94, 399, -123, 2, 0.28864267688603085, -4.533323527949861, x1, x2);
+	uni_test_sqrt_solver_main(6, 70, 140, 70, 1, -1, NAN, x1, x2);
+	uni_test_sqrt_solver_main(7, 84, -3, 12, 0, NAN, NAN, x1, x2);
+	uni_test_sqrt_solver_main(8, 36, - 1245, 4119, 2, 30.87787546251713, 3.705457870816206, x1, x2);
+	uni_test_sqrt_solver_main(9, 3124, -12389741, 1234, 2, 3965.9861359968572, 0.00009959853324614627, x1, x2);
+	uni_test_sqrt_solver_main(10, 0.123, -0.9812, 0.00013456, 2, 7.977098631801949, 0.00013714055577466108, x1, x2);
+	return 0;
 }
 
 int main()
@@ -278,11 +242,14 @@ int main()
 	double x1 = NAN;
 	double x2 = NAN;
 
-	Uni_Test_sqrt_solver();
 	printf("print numbers a b c\n");
 	int nTry_to_enter = 3;
 	fn_safe_scanf(nTry_to_enter, &a, &b, &c);
 	int nRoot = fn_sq_solver(a, b, c, &x1, &x2);
-	PrintfResult(nRoot);
+	printf("x1 = %lf x2 = %lf", x1, x2);
+	printf("\n\n");
+	ft_uni_tests();
+
+	print_result(nRoot);
 	return 0;
 }
